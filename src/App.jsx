68e88c8a2fda315672db8bed71fdde018c4cb916
Leonardo Teixeira <leonardot@wswork.com.br>
2022-06-22 00:42:28 -0300

    Implementação por único array

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 const keys = [
   'Q',
@@ -31,31 +31,70 @@ const keys = [
   'BACKSPACE',
 ]
 
-const emptyWords = [[], [], [], [], [], []]
+const emptyLetter = { letter: '', status: 'NOT-VERIFIED' }
+
+//NOT-VERIFIED, WRONG-POSITION, RIGHT-POSITION
 
 const App = () => {
-  const [words, setWords] = useState(emptyWords)
-  // let currentWord = 0
-  //talvez isso n funcione, acho q tem q ser um state
-  const [currentWord, setCurrentWord] = useState(0)
+  const [letters, setLetters] = useState([])
+  const [currentWord, setCurrentWord] = useState(1)
+  const [isEnterPressed, setIsEnterPressed] = useState(false)
 
-  const handleBackspace = () => {
-    const wordsClone = [...emptyWords]
-    wordsClone[currentWord].pop()
-    setWords(wordsClone)
+  const handleValidateWord = () => {
+    console.log(letters.slice((currentWord - 1) * 5, (currentWord - 1) * 5 + 5))
+    letters.slice((currentWord - 1) * 5, (currentWord - 1) * 5 + 5).forEach((letter, index) => {
+      console.log({ index })
+      console.log({ indexInLetter: index * currentWord })
+      checkIfIsInWord(letter, index * currentWord)
+    })
+  }
+
+  const checkIfIsInWord = (letter, slicedIndex) => {
+    // console.log(letter)
+    // console.log(slicedIndex)
+    if ('WORDY'.includes(letter)) {
+      setLetters(
+        letters.map((letter, index) => (index === slicedIndex ? { letter: letter, status: 'WRONG-POSITION' } : letter))
+      )
+    }
   }
 
   const handleKeyPress = (key) => {
-    if (currentWord > 5) return
-
     if (key === 'BACKSPACE') {
-      handleBackspace()
-    } else if (words[currentWord].length < 5 && key !== 'ENTER') {
-      const wordsClone = [...emptyWords]
-      wordsClone[currentWord].push(key)
-      setWords(wordsClone)
-    } else if (words[currentWord].length === 5 && key === 'ENTER') {
-      setCurrentWord(currentWord + 1)
+      setLetters(letters.slice(0, -1))
+      setCurrentWord(Math.floor(letters.length / 5) + 1)
+    } else if (key === 'ENTER') {
+      setIsEnterPressed(true)
+      handleValidateWord()
+    } else {
+      setLetters([...letters, { letter: key, status: 'NOT-VERIFIED' }])
+      setIsEnterPressed(false)
+      setCurrentWord(Math.floor(letters.length / 5) + 1)
+    }
+  }
+
+  const checkIsKeyDisabled = (key) => {
+    if (letters.length >= 30 && isEnterPressed) return true
+    else if (key === 'ENTER' && isEnterPressed) return true
+    else if (key === 'BACKSPACE' && isEnterPressed) return true
+    else if (letters.length >= currentWord * 5 && !isEnterPressed && key !== 'ENTER' && key !== 'BACKSPACE') return true
+    else if (letters.length < currentWord * 5 && key === 'ENTER') return true
+    else if (letters.length / 5 + 1 === currentWord && key === 'BACKSPACE') return true
+    else return false
+  }
+
+  const checkBagroundColor = (letter) => {
+    if (letter?.status) {
+      switch (letter.status) {
+        case 'NOT-VERIFIED':
+          return 'transparent'
+        case 'WRONG-POSITION':
+          return 'yellow'
+        case 'RIGHT-POSITION':
+          return 'green'
+        default:
+          break
+      }
     }
   }
 
@@ -63,37 +102,50 @@ const App = () => {
     <>
       <h1>Wordle</h1>
 
-      {words.map((word, index1) => (
-        <div key={index1}>
-          {word.map((letter, index) => (
+      <div style={{ height: 500, width: 500, display: 'flex', flexWrap: 'wrap' }}>
+        {Array(30)
+          .fill(emptyLetter)
+          .map((_, index) => (
             <div
-              style={{ backgroundColor: `rgb(${30 * index1}, ${30 * index1}, ${30 * index1}` }}
-              key={letter + index}
+              key={index}
+              style={{
+                border: '1px solid black',
+                boxSizing: 'border-box',
+                width: 'calc(100% / 5)',
+                height: 'calc(100% / 6)',
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
+                backgroundColor: checkBagroundColor(letters[index]),
+              }}
             >
-              {letter}
+              <span>{letters[index]?.letter}</span>
             </div>
           ))}
-        </div>
-      ))}
+      </div>
 
-      <Keyboard handleKeyPress={handleKeyPress} />
+      {keys.map((key, index) => (
+        <button
+          key={key + index}
+          onClick={() => {
+            handleKeyPress(key)
+          }}
+          disabled={checkIsKeyDisabled(key)}
+        >
+          {key}
+        </button>
+      ))}
+      <button
+        onClick={() => {
+          setLetters([])
+          setCurrentWord(1)
+          setIsEnterPressed(false)
+        }}
+      >
+        reset
+      </button>
     </>
   )
 }
 
 export default App
-
-const Keyboard = ({ handleKeyPress }) => (
-  <>
-    {keys.map((key, index) => (
-      <button
-        key={key + index}
-        onClick={() => {
-          handleKeyPress(key)
-        }}
-      >
-        {key}
-      </button>
-    ))}
-  </>
-)
